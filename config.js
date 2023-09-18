6ec71acdd1a76bbfd8b6d217192b0975996041fc
Joven Rosales <jaybenrosales12@gmail.com>
2023-09-18 10:11:39 +0800

    UPdated

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    titleWeb: "Secrettt",
+    introTitle: 'Irish',
+    introDesc: `You're the prettiest girl I've ever seen. Your eyes are beautiful, and your smile is captivating. I admire your bravery.`,
+    btnIntro: 'For More details press me',
+    title: 'Can I be your Boyfriend?',
+    desc: 'I fell in love with at First sight ',
+    btnYes: 'Yes',
+    btnNo: 'No',
+    question: 'There are more than 7 billion people in the world, why do you love me',
+    btnReply: 'Send',
+    mess: "I'll do my best",
+    messDesc: "I love you!!",
+    btnAccept: 'UwU',
+    messLink: 'https://www.facebook.com/JayBen.Selosar' 
+}
